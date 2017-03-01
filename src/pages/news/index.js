563a2aecc51437787d5c3101aeea88663a06f1cf
Amitava Saha <amitava82@gmail.com>
2017-03-01 18:32:44 +0530

    min height

--- src/pages/news/index.js
+++ src/pages/news/index.js
@@ -27,7 +27,7 @@ export default class News extends React.Component {
             <DocumentTitle title={title}>
                 <div className="page-template page-template-page-current">
                     <Header lang={lang}/>
-                    <div id="blog">
+                    <div id="blog" style={{minHeight: 600}}>
 
                     </div>
                     <Footer lang={lang}/>
